5dcb4c80bd488b381433693177ac216f954612f2
Valentin Ogg Padurean <912518+padurean@users.noreply.github.com>
2018-04-13 18:11:53 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Source code for homemade desserts blog [TastyTaste.ro](https://tastytaste.ro)
---
+++ js/main-v1.js
@@ -0,0 +1,498 @@
+var products = [
+  {
+    id: '2018-04-10-tort-cu-ciocolata-fistic-si-zmeura',
+    productCode: 'tort-8',
+    productType: 'tort',
+    name: 'Tort cu Ciocolată, Fistic și Zmeură',
+    tags: [ 'ciocolata', 'fistic', 'zmeura', 'mirrorglaze' ],
+    images: [
+      'https://farm1.staticflickr.com/887/26510394557_774af7b8cf_o.jpg',
+      'https://farm1.staticflickr.com/793/41338477592_0c1e181fd1_o.jpg'
+    ]
+  },
+  {
+    id: '2018-03-24-tort-cu-mango-si-citrice',
+    productCode: 'tort-7',
+    productType: 'tort',
+    name: 'Torturi cu Mango și Citrice',
+    tags: [ 'mango', 'citrice', 'fructe' ],
+    images: [
+      'https://farm1.staticflickr.com/899/41315746652_0d22a6bdc3_o.jpg',
+      'https://farm1.staticflickr.com/787/41359098141_c9942b6c5d_o.jpg',
+      'https://farm1.staticflickr.com/894/27488147868_c9942b6c5d_o.jpg',
+      'https://farm1.staticflickr.com/822/40645103764_8a15e2a2af_o.jpg',
+      'https://farm1.staticflickr.com/788/26487783407_bbc178e2b0_o.jpg',
+      'https://farm1.staticflickr.com/791/26487782957_43787dd228_o.jpg',
+      'https://farm1.staticflickr.com/884/27488147948_1893a059a5_o.jpg',
+      'https://farm1.staticflickr.com/864/27488148068_6e390f71f8_o.jpg',
+      'https://farm1.staticflickr.com/891/40645103494_f057e24b52_o.jpg'
+    ]
+  },
+  {
+    id: '2017-11-03-tort-cu-ciocolata-fistic-si-zmeura',
+    productCode: 'tort-6',
+    productType: 'tort',
+    name: 'Tort cu Ciocolată, Fistic și Zmeură',
+    tags: [ 'ciocolata', 'fistic', 'zmeura', 'mirrorglaze' ],
+    images: [
+      'https://farm1.staticflickr.com/878/27489509848_68dee7a4c5_o.jpg',
+      'https://farm1.staticflickr.com/789/39550830990_ffc17c25db_o.jpg'
+    ]
+  },
+  {
+    id: '2017-11-02-tort-cu-mousse-de-ciocolata',
+    productCode: 'tort-5',
+    productType: 'tort',
+    name: 'Tort cu Mousse de Ciocolată',
+    tags: [ 'ciocolata', 'nuca' ],
+    images: [
+      'https://farm1.staticflickr.com/889/39550902270_4042f07c48_o.jpg',
+      'https://farm1.staticflickr.com/790/26489125047_aa24e90cd8_o.jpg',
+      'https://farm1.staticflickr.com/790/26489124147_5e8aecc0f4_o.jpg'
+    ]
+  },
+  {
+    id: '2017-10-28-tort-cu-ciocolata-fistic-si-zmeura',
+    productCode: 'tort-4',
+    productType: 'tort',
+    name: 'Tort cu Ciocolată, Fistic și Zmeură',
+    tags: [ 'ciocolată', 'fistic', 'zmeura', 'mirrorglaze' ],
+    images: [
+      'https://farm1.staticflickr.com/899/39550990030_e1ebef8612_o.jpg',
+      'https://farm1.staticflickr.com/784/40646929774_6d130d2f92_o.jpg',
+      'https://farm1.staticflickr.com/817/40465322695_c6aa4e1188_o.jpg'
+    ]
+  },
+  {
+    id: '2017-10-03-tort-pavlova',
+    productCode: 'tort-3',
+    productType: 'tort',
+    name: 'Tort Pavlova',
+    tags: [ 'bezea', 'vanilie', 'fructe' ],
+    images: [
+      'https://farm1.staticflickr.com/807/41360886891_221f1cc2a1_o.jpg',
+      'https://farm1.staticflickr.com/888/41360886331_ee1b4a93b7_o.jpg'
+    ]
+  },
+  {
+    id: '2017-09-30-tort-cu-mango-si-citrice',
+    productCode: 'tort-2',
+    productType: 'tort',
+    name: 'Tort cu Mango și Citrice',
+    tags: [ 'mango', 'fructe', 'citrice', 'mousse' ],
+    images: [
+      'https://farm1.staticflickr.com/893/26489343817_2dc03dff1c_o.jpg',
+      'https://farm1.staticflickr.com/788/26489344027_917b4face1_o.jpg',
+      'https://farm1.staticflickr.com/893/26489344067_d3e613e15f_o.jpg'
+    ]
+  },
+  {
+    id: '2017-09-30-tort-cu-ciocolata-si-caramel',
+    productCode: 'tort-1',
+    productType: 'tort',
+    name: 'Tort cu Ciocolată și Caramel',
+    tags: [ 'ciocolata', 'caramel' ],
+    images: [
+      'https://farm1.staticflickr.com/875/40465448535_9ddd996032_o.jpg',
+      'https://farm1.staticflickr.com/791/39551166880_f2748848a1_o.jpg'
+    ]
+  }
+];
+
+function removeHash() { 
+  var scrollV, scrollH, loc = window.location;
+  if ("pushState" in history)
+    history.pushState("", document.title, loc.pathname + loc.search);
+  else {
+    // Prevent scrolling by storing the page's current scroll offset
+    scrollV = document.body.scrollTop;
+    scrollH = document.body.scrollLeft;
+
+    loc.hash = "";
+
+    // Restore the scroll offset, should be flicker free
+    document.body.scrollTop = scrollV;
+    document.body.scrollLeft = scrollH;
+  }
+}
+
+var productTemplate =
+  '\n<article class="blog-post">\n' +
+  '\t<a name="{%product-id%}"></a>\n' +
+  '\t<section class="cover">\n' +
+  '\t\t<img src="{%cover-img-url%}" onClick="zoomProduct(\'{%product-id%}\');">\n' +
+  '\t</section>\n' +
+  '\t<header>{%post-title%}</header>\n' +
+  '\t<section class="tags">{%post-tags%}</section>\n' +
+  // '\t<footer><a href="tel:0728056348" class="btn-order-now" title="Pentru comenzi sună la 0728 056 348">Comandă</a></footer>\n' +
+  '</article>';
+
+var blogPostListJq;
+var productsPerPage = 6;
+var page = 0;
+var filteredTags = [];
+function addFilterTag(tag) {
+  filteredTags.push(tag);
+}
+function removeFilterTag(tag) {
+  var removed = false;
+  var updatedTags = jQuery.grep(filteredTags, function(value) {
+    return value !== tag;
+  });
+  removed = updatedTags.length === filteredTags.length - 1;
+  filteredTags = updatedTags;
+  return removed;
+}
+function toggleFilterTag(tag) {
+  var removed = removeFilterTag(tag);
+  if (!removed) {
+    addFilterTag(tag);
+    var updatedLocationSearch;
+    if (window.location.search)
+      updatedLocationSearch = window.location.search + ',' + tag;
+    else
+      updatedLocationSearch = '?tags='+tag;
+    if ("pushState" in history)
+      history.pushState("", document.title, window.location.pathname + updatedLocationSearch);
+    else
+      window.location = window.location.pathname + updatedLocationSearch
+  } else if (window.location.search) {
+    var locationSearchArr = window.location.search.replace('?tags=', '').split(',');
+    if (locationSearchArr && locationSearchArr.length > 0) {
+      locationSearchArr = jQuery.grep(locationSearchArr, function(value) {
+        return value !== tag;
+      });
+    }
+    if ("pushState" in history) {
+      if (!locationSearchArr || locationSearchArr.length === 0)
+        history.pushState("", document.title, window.location.pathname);
+      else
+        history.pushState("", document.title, window.location.pathname + ('?tags=' + locationSearchArr.join(',')));
+    } else {
+      if (!locationSearchArr || locationSearchArr.length === 0)
+        window.location = window.location.pathname;
+      else
+        window.location = window.location.pathname + '?tags=' + locationSearchArr.join(',');
+    }
+  }
+  page = 0;
+  renderProducts(true);
+}
+function removeAllFilterTags() {
+  filteredTags = [];
+  if ("pushState" in history)
+    history.pushState("", document.title, window.location.pathname);
+  else
+    window.location = window.location.pathname
+  page = 0;
+  renderProducts(true);
+}
+var filteredProductIndexes = [];
+function getMaxPage() {
+  var nbProducts = filteredProductIndexes.length === 0 ?
+    products.length : filteredProductIndexes.length;
+  return Math.ceil(nbProducts / productsPerPage) - 1;
+}
+function filterProducts() {
+  if (!productIndexesPerTag.ready) return;
+  var currFilteredProductIndexes = [];
+  for (var iTag = 0; iTag < filteredTags.length; iTag++) {
+    var tag = filteredTags[iTag];
+    var productIndexes = productIndexesPerTag[tag];
+    if (typeof productIndexes !== 'undefined' && productIndexes.length > 0) {
+      if (iTag == 0) {
+        currFilteredProductIndexes = productIndexes.slice();
+      } else if (currFilteredProductIndexes.length > 0) {
+        currFilteredProductIndexes = jQuery.grep(currFilteredProductIndexes, function(value) {
+          return $tastyJq.inArray(value, productIndexes) >= 0;
+        });
+      }
+    }
+  }
+  currFilteredProductIndexes.sort(function(a, b) {
+    if (a > b) return 1;
+    if (a < b) return -1;
+    return 0;
+  });
+  filteredProductIndexes = currFilteredProductIndexes;
+}
+var zoomedProductId = null;
+function setZoomedProductId(productId) {
+  if (productId === null || typeof productIndexPerId[productId] !== 'undefined')
+    zoomedProductId = productId;
+}
+var filterViewSection;
+var filterView;
+function renderProducts(reset) {
+  filterProducts();
+  
+  if (filteredTags.length > 0) {
+    var filtersHtmlArr = [];
+    for (var iTag = 0; iTag < filteredTags.length; iTag++) {
+      var tag = filteredTags[iTag];
+      filtersHtmlArr.push('<a href="/" onclick="event.preventDefault(); toggleFilterTag(\'' + tag + '\')">' + tag + ' &nbsp;<i class="fas fa-xs fa-times"></i></a>');
+    }
+    filterView.html(filtersHtmlArr.join('\n'));
+    filterViewSection.removeClass('hidden');
+  } else {
+    filterViewSection.addClass('hidden');
+    filterView.text('');
+  }
+
+  // find out on which page the zoomed product lies as all pages will be rendered up to that page (including)
+  if (zoomedProductId !== null) {
+    var productIndex = productIndexPerId[zoomedProductId];
+    if (filteredProductIndexes.length > 0) {
+      var filteredProductIndex = $tastyJq.inArray(productIndex, filteredProductIndexes);
+      if (filteredProductIndex < 0 && typeof console !== 'undefined' && typeof console.log === 'function')
+        console.log('productIndex ' + productIndex + ' not in filteredProductIndexes ' + filteredProductIndexes);
+      else
+        page = Math.floor(filteredProductIndex / productsPerPage);
+    } else 
+      page = Math.floor(productIndex / productsPerPage);
+  }
+
+  var nbProducts = filteredProductIndexes.length === 0 ?
+    products.length : filteredProductIndexes.length;
+
+  var htmlArr = [];
+  var startIndex = zoomedProductId !== null ? 0 : page * productsPerPage;
+  for (var i = startIndex; i < nbProducts && i < (page+1) * productsPerPage; i++) {
+    var productIndex = filteredProductIndexes.length === 0 ?
+      i : filteredProductIndexes[i];
+    var p = products[productIndex]
+    var tagsHtmlArr = [];
+    for (var j = 0; j < p.tags.length; j++) {
+      var tag = p.tags[j];
+      var isTagFiltered = $tastyJq.inArray(tag, filteredTags) >= 0;
+      tagsHtmlArr.push(
+        '<a href="" ' + (isTagFiltered ? 'class="filtered-tag"' : '') + ' onclick="event.preventDefault(); toggleFilterTag(\'' + tag + '\')">' +
+        tag +
+        '</a>'
+      );
+    }
+    var mapObj = {
+      '{%product-id%}': p.id,
+      '{%cover-img-url%}': p.images[0],
+      '{%post-title%}': p.name,
+      '{%post-tags%}': tagsHtmlArr.join(' |\n')
+    };
+    var pHtml = productTemplate.replace(
+      /{%product-id%}|{%cover-img-url%}|{%post-title%}|{%post-tags%}/gi,
+      function(matched){ return mapObj[matched]; }
+    );
+    htmlArr.push(pHtml);
+  }
+  if (reset)
+    blogPostListJq.html(htmlArr.join('\n'));
+  else
+    blogPostListJq.append(htmlArr.join('\n'));
+
+  if (page == getMaxPage()) {
+    btnLoadMoreProductsJq.addClass('no-more');
+  } else {
+    btnLoadMoreProductsJq.removeClass('no-more');
+  }
+
+  if (zoomedProductId !== null) {
+    zoomProduct(null);
+  } else if (reset) {
+    var topPx = filteredTags.length > 0 ? filterViewSection.offset().top - 5 : 0;
+    if (typeof window.scroll === 'function') {
+      window.scroll({ top: topPx, behavior: "smooth" });
+    } else {
+      window.scrollTo(0, topPx);
+    }
+  }
+}
+
+var zoomedProductSliderState = {
+  productIndex: 0,
+  position: 0,
+};
+var zoomedProductModalContentJq;
+var zoomedProductModalCaptionJq;
+function showPrevImage(e) {
+  if (typeof e !== 'undefined') e.preventDefault();
+  var product = products[zoomedProductSliderState.productIndex];
+  var position = zoomedProductSliderState.position;
+  if (position === 0) return;
+  position--;
+  zoomedProductModalCaptionJq.text((position+1) + ' / ' + product.images.length);
+  zoomedProductModalContentJq.html('<img src="' + product.images[position] + '">');
+  if (imgNavTipGoPrevShown)
+    hideImgNavTip();
+  zoomedProductSliderState.position = position;
+}
+function showNextImage(e) {
+  if (typeof e !== 'undefined') e.preventDefault();
+  var product = products[zoomedProductSliderState.productIndex];
+  var position = zoomedProductSliderState.position;
+  if (position === product.images.length-1) return;
+  position++;
+  zoomedProductModalCaptionJq.text((position+1) + ' / ' + product.images.length);
+  zoomedProductModalContentJq.html('<img src="' + product.images[position] + '">');
+  if (imgNavTipGoNextShown)
+    hideImgNavTip();
+  if (!imgNavTipGoPrevShown && position === product.images.length-1)
+    showImgNavTip(false);
+  zoomedProductSliderState.position = position;
+}
+function showPrevOrNextProductImage(e) {
+  var modalContentJq = $tastyJq(this);
+  var pWidth = modalContentJq.innerWidth(); //use .outerWidth() if you want borders
+  var pOffset = modalContentJq.offset(); 
+  var x = e.pageX - pOffset.left;
+  var clickedOnLeftHalf = pWidth/2 > x;
+
+  var product = products[zoomedProductSliderState.productIndex];
+  var position = zoomedProductSliderState.position;
+  
+  if (
+    (clickedOnLeftHalf && position === 0) ||
+    (!clickedOnLeftHalf && position === product.images.length-1)
+  ) return;
+
+  if(clickedOnLeftHalf) // clicked on the left half
+    position--;
+  else // clicked on the right half
+    position++;
+
+  zoomedProductModalCaptionJq.text((position+1) + ' / ' + product.images.length);
+  zoomedProductModalContentJq.html('<img src="' + product.images[position] + '">');
+  
+  if((clickedOnLeftHalf && imgNavTipGoPrevShown) || (!clickedOnLeftHalf && imgNavTipGoNextShown))
+    hideImgNavTip();
+  if (!imgNavTipGoPrevShown && !clickedOnLeftHalf && position === product.images.length-1)
+    showImgNavTip(false);
+
+  zoomedProductSliderState.position = position;
+}
+
+var imgNavTipJq;
+var imgNavTipGoNextJq;
+var imgNavTipGoPrevJq;
+var imgNavTipGoNextShown = false;
+var imgNavTipGoPrevShown = false;
+function showImgNavTip(next) {
+  if (next) {
+    imgNavTipGoPrevJq.addClass('hidden');
+    imgNavTipGoNextJq.removeClass('hidden');
+    imgNavTipGoNextShown = true;
+  } else {
+    imgNavTipGoNextJq.addClass('hidden');
+    imgNavTipGoPrevJq.removeClass('hidden');
+    imgNavTipGoPrevShown = true;
+  }
+  imgNavTipJq.removeClass('hidden');
+}
+function hideImgNavTip() {
+  imgNavTipJq.addClass('hidden');
+  imgNavTipGoNextJq.addClass('hidden');
+  imgNavTipGoPrevJq.addClass('hidden');
+}
+
+var zoomedProductModalJq;
+var documentBodyJq;
+function zoomProduct(productId) {
+  if (productId)
+    setZoomedProductId(productId);
+  if (zoomedProductId !== null) {
+    var productIndex = productIndexPerId[zoomedProductId];
+
+    zoomedProductSliderState.productIndex = productIndex
+    zoomedProductSliderState.position = 0;
+
+    var p = products[zoomedProductSliderState.productIndex];
+    zoomedProductModalJq.removeClass('hidden');
+    documentBodyJq.addClass('overflow-hidden');
+    window.location.hash = p.id;
+
+    var product = products[zoomedProductSliderState.productIndex];
+    var position = zoomedProductSliderState.position;
+
+    zoomedProductModalCaptionJq.text((position+1) + ' / ' + product.images.length);
+    zoomedProductModalContentJq.html('<img src="' + product.images[0] + '">');
+    
+    if (!imgNavTipGoNextShown)
+      showImgNavTip(true);
+  }
+}
+function zoomProductOut() {
+  zoomedProductId = null;
+  zoomedProductModalJq.addClass('hidden');
+  documentBodyJq.removeClass('overflow-hidden');
+  removeHash();
+}
+
+var $tastyJq = jQuery.noConflict();
+var btnLoadMoreProductsJq;
+var productIndexPerId = {
+  ready: false
+};
+var productIndexesPerTag = {
+  ready: false
+}
+$tastyJq(document).ready(function() {
+  blogPostListJq = $tastyJq('#blog-post-list');
+  zoomedProductModalJq = $tastyJq('#zoomed-product-modal');
+  documentBodyJq = $tastyJq(document.body);
+  btnLoadMoreProductsJq = $tastyJq('#btn-load-more-products');
+
+  for (var i = 0; i < products.length; i++) {
+    var p = products[i];
+    productIndexPerId[p.id] = i;
+
+    for (var iTag = 0; iTag < p.tags.length; iTag++) {
+      var tag = p.tags[iTag];
+      if (typeof productIndexesPerTag[tag] === 'undefined')
+        productIndexesPerTag[tag] = [];
+      productIndexesPerTag[tag].push(i);
+    }
+  }
+  productIndexPerId.ready = true;
+  productIndexesPerTag.ready = true;
+
+  zoomedProductModalContentJq = $tastyJq('#zoomed-product-modal-content')
+  zoomedProductModalContentJq.on('click', showPrevOrNextProductImage);
+  if ($tastyJq.support.touch) {
+    zoomedProductModalContentJq.on('swipeleft', showNextImage);
+    zoomedProductModalContentJq.on('swiperight', showPrevImage);
+  }
+  zoomedProductModalCaptionJq = $tastyJq('#zoomed-product-modal-caption').find('span');
+  imgNavTipJq = $tastyJq('#img-nav-tip');
+  imgNavTipGoNextJq = $tastyJq('#img-nav-tip-go-next');
+  imgNavTipGoPrevJq = $tastyJq('#img-nav-tip-go-prev');
+
+  var locationSearch =
+    window.location.search && window.location.search.replace('?tags=', '');
+  locationSearch = locationSearch && locationSearch.split(',');
+  if (locationSearch) {
+    filteredTags = locationSearch.slice(); 
+  }
+
+  setZoomedProductId(window.location.hash ? window.location.hash.replace('#', '') : null);
+  if (zoomedProductId == null)
+    removeHash();
+
+  filterViewSection = $tastyJq('#filter-view-section');
+  filterView = $tastyJq('#filter-view');
+
+  renderProducts(false);
+
+  $tastyJq('.current-year').text((new Date()).getFullYear());
+
+  btnLoadMoreProductsJq.on('click', function(e) {
+    e.preventDefault();
+    if (page < getMaxPage()) {
+      page++;
+      renderProducts(false);
+    }
+  });
+
+  var spinnerSectionJq = $tastyJq('#spinner-section');
+  var loadMoreProductsSectionJq = $tastyJq('#load-more-products-section');
+  spinnerSectionJq.addClass('hidden');
+  blogPostListJq.removeClass('hidden');
+  loadMoreProductsSectionJq.removeClass('hidden');
+});
